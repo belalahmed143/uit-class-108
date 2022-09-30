0ef2269603f5615145e0b6a41b780960c4b48a90
unknown <mohammadbelalahmed82@gmail.com>
2022-09-30 21:48:12 +0600

    first commit

---
+++ class1.py
@@ -0,0 +1,27 @@
+print('Hello')
+
+
+"""
+hh
+hs 
+dd
+
+"""
+# @#$%%^^&
+# 1hjhh
+
+name ='belal3'
+Name ='belal2'
+roll = 5
+reg = 5555
+full_name = 'Mohammad Belal Hossain'
+
+
+a = 4
+A = 5
+# b = str(a)
+print(type(str(a)))
+
+name = 'Belal'
+
+
