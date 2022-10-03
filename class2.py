bd41197c5e608d2408fbf9d12b4597445c71e1fe
unknown <mohammadbelalahmed82@gmail.com>
2022-10-03 22:36:32 +0600

    2nd class

---
+++ class2.py
@@ -0,0 +1,126 @@
+
+# DataTypes
+
+# a = 5
+# A = 4.5
+
+# c = a + A
+# print(type(c)) 
+# print(c)
+
+
+# s = str(c)
+# print(type(s))
+
+# print(type(a + A))
+
+
+# num = 5.5
+
+# print(type(num))
+
+
+# x = '5'
+# print(type(x))
+# print(x)
+
+# y = int(x)
+# print(type(y))
+# print(y)
+
+# z = float(y)
+# print(type(z))
+# print(z)
+
+# yy = bool(z)
+# print(type(yy))
+# print(yy)
+
+
+
+# Operator
+
+# a = 5 + 3
+
+# print(a)
+
+# b = 5-3
+
+# print(b)
+
+# c = 5 * 3
+
+# print(c)
+
+# d = 5 % 3
+# print(d)
+
+# e = 5 ** 3
+# print(e)
+
+# f = 15 // 3
+# print(f)
+
+# a = 0
+# r = 3
+
+# a += r
+
+# a = a + r
+
+# a = r
+
+# a = a+r
+# print(a)
+
+
+
+# d = 3
+
+# if d == 3:
+#     print('Ok 3')
+# elif d == 4:
+#     print('Ok 4')
+# elif d == 7:
+#     print('Ok 7')
+# else:
+#     print('No result')
+
+
+
+
+# x = 3
+# y = 2
+# if x != y:
+#     print('Not Equal')
+# else:
+#     print('Equal')
+
+
+
+# num1 = 11
+
+# if num1 > 12:
+#     print('num1 grater than 7')
+# elif num1 > 10:
+#     print('num1 grater than 10')
+# elif num1 > 6:
+#     print('num1 grater than 6')
+
+
+
+a = 10
+
+if a >= 10 and a < 14:
+    print('Ok')
+else:
+    print('no ok')
+
+    
+
+
+
+
+
+
+
