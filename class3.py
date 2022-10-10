e0db1b7b18905b0752b58c6dde45681f6336fd6e
unknown <you@example.com>
2022-10-10 22:14:32 +0600

    4th class

---
+++ class3.py
@@ -0,0 +1,118 @@
+# &=
+# %=
+# |=
+
+# a = 3
+# if a == 3 or a == 3:
+#     print(a)
+
+# # 16,8,4,2,1
+# # 5 = 101
+# # 4 = 100
+# # ------------
+#        100 =4
+
+         
+
+# a = 5
+# b = 4
+
+# a = a & b
+# print(a)
+
+
+# a = a | 4
+# print(a)
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+
+# for a in range(5,101,3):
+#     print(a)
+
+name = 'Belal'
+dep = 'computer'
+
+# my_list = ['Belal', 'computer']
+# print(my_list)
+
+# name = 'Belal'
+# s_id = 100
+# dep = 'Computer'
+
+# print(name)
+# print(s_id)
+# print(dep)
+
+
+
+# s_profile = ['Belal',100, 'Computer']
+
+# print(s_profile[1:2])
+
+# print(s_profile)
+
+
+# for a in s_profile:
+#     print(a)
+
+
+# s_profile = ['Belal',100, 'Computer']
+
+# my_tuple = ('onion', 'potato', 'ginger','onion', 'potato', 'ginger','potato')
+
+# print(type(len(my_tuple)))
+# print(my_tuple)
+
+# print(my_tuple.count('potato'))
+
+
+# count_potato = my_tuple.count('potato')
+# print(count_potato)
+
+
+# print(my_tuple[0])
+# print(my_tuple[1])
+# print(my_tuple[2])
+
+# for i in my_tuple:
+#     print(i)
+
+# for a in len(my_tuple):
+#     print(my_tuple[a])
+
+# for a in my_tuple:
+#     print(a)
+
+# my_set = {'onion', 'potato', 'ginger'}
+# my_set.remove('potato')
+# my_set.remove('potatos')
+# my_set.discard('potatos')
+
+# print(my_set)
+
+# sum = 0
+
+# for s in my_set:
+#     print(s)
+
+# my_dic = {'name' : 'Md Belal', 'dep':'CSC','roll':54}
+# print(my_dic.copy())
+# print(my_dic)
+# print(my_dic['dep'])
+
+
+name = 'mohammad belal'
+print(name[8])
+
+my_bool = False
+
+print(type(my_bool))
+
+
+
+
+
+
---
+++ class4.py
@@ -0,0 +1,84 @@
+# s1 = 40 + 15
+# print(s1)
+
+# s2 = 60+18
+# print(s2)
+
+# def student(a,b):
+#     # return print('result : ', a+b)
+#     result = a + b
+#     #  print(result)
+#     return result
+
+
+# s1 = student(55,17)
+# print('s1 : ', s1)
+
+# s2 = student(75,17)
+# print('s2 : ', s2)
+
+# s3 = student(73,17)
+# print('s3 : ', s3)
+
+
+# def teacher(name):
+#     return name
+
+# t1 = teacher('Belal')
+
+# print('teacher name : ',t1)
+
+# def person(name,s_id,dep):
+#     return name, s_id, dep
+
+# def student(std_name,std_id,std_dept='Computer'):
+#     return std_name,std_id,std_dept
+
+# print(student('Hasan','4353'))
+# print(student('Hasan1','43533'))
+# print(student('Hasan2','43534'))
+
+
+# print(student('Ashraf',123,'EEE'))
+# print(student('Omar Faruk ',435.3,'CIA'))
+# print(student('Hasan Ali','4353','BBA'))
+# print(student('Ranga','8788','CSE'))
+
+# n =int(input('Enter Your number : '))
+
+# sum = 0
+# for i in range(1,n):
+#     sum += i
+
+# print('Result : ', sum)
+
+
+# def shop_total_sale(*args):
+#     print(type(args))
+#     sum = 0
+#     for amount in args:
+#         sum = sum + amount
+#     return sum
+
+# total_amount = shop_total_sale(54,67,23,46,78,90,11,34,56,34)
+
+# print('Total Sale Amount : ', total_amount)
+
+
+
+def add(**kwargs):
+    print(type(kwargs))
+    sum = 0
+    for key in kwargs:
+        sum = sum + kwargs[key]
+    return sum
+total = add(a=54,b=67,c=23,d=46)
+print(total)
+
+
+# @add
+# def add2():
+#     pass
+
+
+
